65f1a93d2d77c6cee60bb30a1647ef712ffb6796
Electric Light <electric.light.53@gmail.com>
2019-04-07 19:25:42 -0300

    Adjust deployment compliance and add timestamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,34 @@
 $(document).ready( function () {
 
-    $.getJSON("../../../crawler/data/subreddit-data.json", function(data) {
+    $.getJSON("../../../data/subreddit-data.json", function(data) {
 
-        let table = []
-
-        for (let i = 0; i < data.subreddits.length; i++) {
-            let row = []
-            row.push(data.subreddits[i].name)
-            row.push(data.subreddits[i].description)
-            row.push(data.subreddits[i].members)
-            row.push(data.subreddits[i].age)
-            row.push(data.subreddits[i].nsfw)
-            row.push(data.subreddits[i].recent_submissions)
-            row.push(data.subreddits[i].moderators.length)
-            table.push(row);
-        }
-
-        $('#subreddit-table').DataTable( {
-            data: table
-        } );
+        updateDate(data);
+        updateTable(data);
 
     });
 
 });
+
+function updateDate(data) {
+    $('#date').html(data.date);
+}
+
+function updateTable(data) {
+    let table = [];
+
+    for (let i = 0; i < data.subreddits.length; i++) {
+        let row = []
+        row.push(data.subreddits[i].name)
+        row.push(data.subreddits[i].description)
+        row.push(data.subreddits[i].members)
+        row.push(data.subreddits[i].age)
+        row.push(data.subreddits[i].nsfw)
+        row.push(data.subreddits[i].recent_submissions)
+        row.push(data.subreddits[i].moderators.length)
+        table.push(row);
+    }
+
+    $('#subreddit-table').DataTable({
+        data: table
+    });
+}
